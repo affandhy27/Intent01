0d577a33bc0ad9d8984f45da1d85aa629b6efce3
Affandhy <02_xirpl3.smktelkom-mlg.sch.id>
2016-10-16 20:21:15 +0700

    onBackPressed part1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent01/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent01/HasilActivity.java
@@ -2,12 +2,31 @@ package id.sch.smktelkom_mlg.learn.intent01;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HasilActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR, 0);
+
+        int yearsnow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunlahir = yearsnow - umur;
+
+        setTitle("Hasil");
+
+        TextView tvhasil = (TextView) findViewById(R.id.textview);
+        tvhasil.setText(nama + " lahir pada tahun " + tahunlahir);
+
     }
-}
